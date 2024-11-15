adb4742a147a65d2a6327d5d6c764cc499e934e9
Danqi74 <dan.hnyp@gmail.com>
2024-11-15 15:36:05 +0200

    Change feedback text

--- src/Components/Feedback/Feedback.jsx
+++ src/Components/Feedback/Feedback.jsx
@@ -5,15 +5,15 @@ function Feedback(){
         <div class="clients_profiles">
             <div class="client"><p class="client_text">Мені 4 кебаби з понячого мʼяса</p>
             <div class="client_info">
-                <p class="client_name">Назарко ************</p>
+                <p class="client_name">Назарко Сеньковський</p>
             </div>
             </div>
             <div class="client"><p class="client_text">Мʼясо свіже, навіть занадто, я хотів зробити стейк, а мʼясо дивилось на мене і благало зупинитись</p>
                 <div class="client_info">
-                <p class="client_name">Юрась *******</p>
+                <p class="client_name">Юрась Пристай</p>
                 </div>
             </div>
-            <div class="client"><p class="client_text">Рекомендую їсти перед сном</p>
+            <div class="client"><p class="client_text">До мне прийшов клієнт і сказав шо мʼясо говорить. Я відправив його в дурку</p>
                 <div class="client_info">
                 <p class="client_name">Невідомий студент психологічного</p>
                 </div>
